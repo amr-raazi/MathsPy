5d15ac5f02227b317ad9f89a09ebf294c5485880
amr-raazi <50888029+amr-raazi@users.noreply.github.com>
2020-09-26 13:14:00 +0530

    Typo fixed.

--- tic tac toe.py
+++ tic tac toe.py
@@ -1,45 +1,59 @@
 from tkinter import *
 from tkinter import font as tkfont
 
+# program variables
 tic_tac_toe = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-root = Tk()
-root.title("Tic Tac toe")
-button_font = tkfont.Font(family="Times", size="24", weight="bold", slant="italic")
 turn = 1
 winner = 0
 
+# tkinter variables
+root = Tk()
+root.title("Tic Tac toe")
+button_font = tkfont.Font(family="Times", size="24", weight="bold", slant="italic")
+
+# top row
 top_one = Button(root, text="", command=lambda: click_button(0, 0), width=7, height=2, font=button_font)
 top_two = Button(root, text="", command=lambda: click_button(0, 1), width=7, height=2, font=button_font)
 top_three = Button(root, text="", command=lambda: click_button(0, 2), width=7, height=2, font=button_font)
 
+# middle row
 middle_one = Button(root, text="", command=lambda: click_button(1, 0), width=7, height=2, font=button_font)
 middle_two = Button(root, text="", command=lambda: click_button(1, 1), width=7, height=2, font=button_font)
 middle_three = Button(root, text="", command=lambda: click_button(1, 2), width=7, height=2, font=button_font)
 
+# bottom row
 bottom_one = Button(root, text="", command=lambda: click_button(2, 0), width=7, height=2, font=button_font)
 bottom_two = Button(root, text="", command=lambda: click_button(2, 1), width=7, height=2, font=button_font)
 bottom_three = Button(root, text="", command=lambda: click_button(2, 2), width=7, height=2, font=button_font)
 
+# button order
 button_order = [[top_one, top_two, top_three], [middle_one, middle_two, middle_three],
                 [bottom_one, bottom_two, bottom_three]]
 
 
+# click button
 def click_button(x, y):
     global turn
+    # check if it is taken
     if tic_tac_toe[x][y] != 0:
         return
+    # set in matrix
     tic_tac_toe[x][y] = turn
+    # set button text and change turn
     if turn == 1:
         button_order[x][y]["text"] = "X"
         turn = 2
     else:
         button_order[x][y]["text"] = "O"
         turn = 1
+    # check win conditions
     evaluate()
 
 
+# eveluate win conditions
 def evaluate():
     global tic_tac_toe, winner
+  #  print(tic_tac_toe[1][1])
     # horizontal check
     for line in tic_tac_toe:
         if line.count(1) == 3 or line.count(2) == 3:
@@ -49,13 +63,16 @@ def evaluate():
         if tic_tac_toe[0][x] == tic_tac_toe[1][x] == tic_tac_toe[2][x]:
             winner = tic_tac_toe[0][x]
     # diagonal checking
-    if tic_tac_toe[0][0] == tic_tac_toe[1][1] == tic_tac_toe[2][2] == 1 or tic_tac_toe[0][2] == tic_tac_toe[1][1] == \
+    if tic_tac_toe[0][0] == tic_tac_toe[1][1] == tic_tac_toe[2][2] or tic_tac_toe[0][2] == tic_tac_toe[1][1] == \
             tic_tac_toe[2][0]:
         winner = tic_tac_toe[1][1]
+        print(winner)
+    # check if a player won
     if winner != 0:
         winning_sequence()
 
 
+# win sequence
 def winning_sequence():
     global winner, root
     if winner == 1:
@@ -69,6 +86,7 @@ def winning_sequence():
     root.mainloop()
 
 
+# pack in grid
 top_one.grid(row=1, column=0)
 top_two.grid(row=1, column=1)
 top_three.grid(row=1, column=2)
